55a40fca29fddb1579a72afcffe2a969ade6699e
Youngjin Kim <steven.kimijin@gmail.com>
2021-01-03 22:17:51 +0900

    update maze

--- java-correct/maze.java
+++ java-correct/maze.java
@@ -1,37 +1,57 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class maze {
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args){
         int[][] arr = {{1,0,1,0,1,0},{1,1,1,1,1,1},{0,0,0,0,0,1},{1,1,1,1,1,1},{1,1,1,1,1,1}};
-        boolean[][] check = new boolean[arr.length][arr[0].length];
-        int cnt = 0;
-        
-        for(int i=0; i<arr.length ;i++){
-            for(int j=0; j<arr[i].length ;j++){
-                if(bfs(i,j,arr,check)){
-                    cnt++;
+        System.out.println(String.format("answer : %d", bfs(0, 0, arr)));
+    }
+
+    static int bfs(int x, int y, int[][] arr){
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(x, y));
+
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            x = node.getX();
+            y = node.getY();
+
+            // i<4 : 4가지 방향을 의미
+            for(int i=0; i<4 ;i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= arr.length || ny >= arr[x].length) continue;
+
+                if(arr[nx][ny] == 0) continue;
+
+                // 최단거리일 경우에만 길을 찾아서 +1을 해줌
+                if(arr[nx][ny] == 1){
+                    arr[nx][ny] = arr[x][y] + 1;
+                    queue.offer(new Node(nx, ny));
                 }
             }
         }
+        return arr[arr.length-1][arr[x].length-1];
+    }
+}
 
-        System.out.println(String.format("cnt : %d", cnt));
+class Node {
+    private int x;
+    private int y;
+
+    Node(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 
-    static boolean bfs(int x, int y, int[][] arr, boolean[][] check){
-
-        if(x < 0 || y < 0 || x >= 5 || y >= 6){
-            return false;
-        }
-
-        if(check[x][y] == false && arr[x][y] != 0){
-
-            check[x][y] = true;
-
-            bfs(x+1, y, arr, check);
-            bfs(x-1, y, arr, check);
-            bfs(x, y+1, arr, check);
-            bfs(x, y-1, arr, check);
-        }
-
-        return false;
+    public int getX(){
+        return this.x;
     }
 
+    public int getY(){
+        return this.y;
+    }
 }
